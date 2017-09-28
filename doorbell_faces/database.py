b3381f1f00a8f175fbb9913853242fa8c641e0f1
Misha Wagner <mishajw@gmail.com>
2017-09-28 15:01:43 +0100

    Add recognition and capture tables to database

--- doorbell_faces/database.py
+++ doorbell_faces/database.py
@@ -12,6 +12,23 @@ def get_database() -> Database:
         name STRING TEXT NULL)
     """)
 
+    database.execute("""
+      CREATE TABLE IF NOT EXISTS capture (
+        capture_id INTEGER PRIMARY KEY,
+        time INTEGER,
+        hash INTEGER)
+    """)
+
+    database.execute("""
+      CREATE TABLE IF NOT EXISTS recognition (
+        person_id INTEGER,
+        capture_id INTEGER,
+        face_embedding BLOB,
+        FOREIGN KEY (person_id) REFERENCES person(person_id),
+        FOREIGN KEY (capture_id) REFERENCES capture(capture_id),
+        PRIMARY KEY (person_id, capture_id))
+    """)
+
     database.commit()
 
     return database

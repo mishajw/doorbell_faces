9581b58f21a2131a66b0f300eebdb2172c3d430f
Misha Wagner <mishajw@gmail.com>
2017-09-29 08:46:27 +0100

    Update requirements.txt and remove unnecessary code from person.py

--- iot_doorbell_face_server/person.py
+++ iot_doorbell_face_server/person.py
@@ -1,22 +1,8 @@
 from iot_doorbell_face_server import recognition
 from typing import Optional, List
-from flask_restful import Resource
 
 
 class Person:
     def __init__(self, name: Optional[str], recognitions: List["recognition.Recognition"]):
         self.name = name
         self.recognitions = recognitions
-
-    def to_json(self) -> dict:
-        return {
-            "name": self.name
-        }
-
-
-class PersonResource(Resource):
-    def __init__(self):
-        self.__name__ = "PersonResource"
-
-    def get(self):
-        return Person("John Smith", []).to_json()

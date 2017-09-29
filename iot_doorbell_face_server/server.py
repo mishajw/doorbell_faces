e523d85e9cb01bf6be0c910700f879b8f6c1316a
Misha Wagner <mishajw@gmail.com>
2017-09-29 10:33:12 +0100

    Change server to use flask.views.View instead of @app.route

--- iot_doorbell_face_server/server.py
+++ iot_doorbell_face_server/server.py
@@ -4,72 +4,93 @@ from iot_doorbell_face_server import database
 from iot_doorbell_face_server import exceptions
 from iot_doorbell_face_server import list_recognitions_handler
 from flask import Flask, request, jsonify, send_file
+from flask.views import View
 import logging
 import time
 
 log = logging.getLogger(__name__)
 
-app = Flask("iot-doorbell-face-server")
-_database = database.get_database()
-
 
 def run(port: int):
+    log.info("Connecting to database")
+    _database = database.get_database()
+
     log.info("Starting server")
+    app = Flask("iot-doorbell-face-server")
+    app.add_url_rule("/add_capture", view_func=AddCaptureView.as_view("add_capture", _database), methods=["POST"])
+    app.add_url_rule("/list_recognitions", view_func=ListRecognitionsView.as_view("list_recognitions", _database))
+    app.add_url_rule("/get_capture", view_func=GetCaptureView.as_view("get_capture", _database))
+    app.errorhandler(exceptions.ServerException)(server_exception_handler)
     app.run(port=port)
 
 
-@app.route("/add_capture", methods=["POST"])
-def add_capture():
-    log.info("add_capture called")
+class AddCaptureView(View):
+    methods = ["POST"]
 
-    time = request.args.get("time", type=int)
-    image_width = request.args.get("image_columns", type=int, default=1920)
-    image_height = request.args.get("image_rows", type=int, default=1080)
-    image_stream = __get_file_stream()
+    def __init__(self, _database: database.Database):
+        self.database = _database
 
-    add_capture_handler.add_capture(time, image_width, image_height, image_stream, _database)
+    def dispatch_request(self):
+        log.info("add_capture called")
 
-    # TODO Return correct value
-    return "success"
+        _time = request.args.get("time", type=int)
+        image_width = request.args.get("image_columns", type=int, default=1920)
+        image_height = request.args.get("image_rows", type=int, default=1080)
+        image_stream = get_file_stream()
 
+        add_capture_handler.add_capture(_time, image_width, image_height, image_stream, self.database)
 
-@app.route("/list_recognitions", methods=["GET"])
-def list_recognitions():
-    log.info("list_recognitions called")
-
-    start_time = request.args.get("start_time", type=int, default=0)
-    end_time = request.args.get("end_time", type=int, default=__get_current_unix_time())
-
-    results = list_recognitions_handler.list_recognitions(start_time, end_time, _database)
-
-    # TODO Check if generic .to_json handler in Flask
-    return jsonify([result.to_json() for result in results])
+        # TODO Return correct value
+        return "success"
 
 
-@app.route("/get_capture", methods=["GET"])
-def get_capture():
-    log.info("get_capture called")
+class ListRecognitionsView(View):
+    methods = ["GET"]
 
-    capture_hash = request.args.get("capture_hash", type=str, default=None)
-    capture_id = request.args.get("capture_id", type=int, default=None)
+    def __init__(self, _database: database.Database):
+        self.database = _database
 
-    if capture_hash is not None:
-        capture_image_path = get_capture_handler.get_capture_from_hash(capture_hash, _database)
-    elif capture_id is not None:
-        capture_image_path = get_capture_handler.get_capture_from_id(capture_id, _database)
-    else:
-        raise exceptions.IncorrectValueException.from_message("One of \"capture_hash\" or \"capture_id\" must be set")
+    def dispatch_request(self):
+        log.info("list_recognitions called")
 
-    return send_file(capture_image_path)
+        start_time = request.args.get("start_time", type=int, default=0)
+        end_time = request.args.get("end_time", type=int, default=get_current_unix_time())
+
+        results = list_recognitions_handler.list_recognitions(start_time, end_time, self.database)
+
+        # TODO Check if generic .to_json handler in Flask
+        return jsonify([result.to_json() for result in results])
 
 
-@app.errorhandler(exceptions.ServerException)
+class GetCaptureView(View):
+    methods = ["GET"]
+
+    def __init__(self, _database: database.Database):
+        self.database = _database
+
+    def dispatch_request(self):
+        log.info("get_capture called")
+
+        capture_hash = request.args.get("capture_hash", type=str, default=None)
+        capture_id = request.args.get("capture_id", type=int, default=None)
+
+        if capture_hash is not None:
+            capture_image_path = get_capture_handler.get_capture_from_hash(capture_hash, self.database)
+        elif capture_id is not None:
+            capture_image_path = get_capture_handler.get_capture_from_id(capture_id, self.database)
+        else:
+            raise exceptions.IncorrectValueException.from_message(
+                "One of \"capture_hash\" or \"capture_id\" must be set")
+
+        return send_file(capture_image_path)
+
+
 def server_exception_handler(server_exception: exceptions.ServerException):
     log.warning("Returning exception to user: %s" % server_exception)
     return jsonify(server_exception.to_json()), server_exception.status_code
 
 
-def __get_file_stream():
+def get_file_stream():
     if "file" not in request.files:
         raise exceptions.IncorrectValueException.from_value_and_explanation(
             "files", list(request.files), "doesn't contain  \"file\"")
@@ -82,5 +103,5 @@ def __get_file_stream():
     return file.stream
 
 
-def __get_current_unix_time() -> int:
+def get_current_unix_time() -> int:
     return int(time.time() * 1000)
